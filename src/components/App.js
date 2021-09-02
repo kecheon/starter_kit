58b13aa8d25ae029f84b365fac2b8fd07007b1fb
kecheon <kecheon@gmail.com>
2021-09-02 16:28:34 +0900

    refactor App with Main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import './App.css';
-import Identicon from 'identicon.js';
 import SocialNetwork from '../abis/SocialNetwork.json';
 import Navbar from './Navbar';
+import Main from './Main';
 
 class App extends Component {
   async componentWillMount() {
@@ -62,44 +62,8 @@ class App extends Component {
     return (
       <div>
         <div className="container-fluid mt-5">
-          <Navbar account={this.state.account} />
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
-              <div className="content mr-auto ml-auto">
-                { this.state.posts.map((post, key) => {
-                    return(
-                      <div className="card mb-4" key={key}>
-                        <div className="card-header">
-                          <img className="ml-2"
-                            width='30'
-                            height='30'
-                            src={`data:image/png;base64, ${ new Identicon(this.state.account, 20).toString() }`}
-                            alt="avatar"
-                          />
-                          <small className="text-muted">{ post.author }</small>
-                        </div>
-                        <ul id="postList" className="list-group list-group-flush">
-                          <li className="list-group-item">
-                            { post.content }
-                          </li>
-                          <li key={key} className="list-group-item py-2">
-                            <small className="float-eft mt-1 text-muted">
-                              TIPS: { window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')} ETH
-                            </small>
-                            <button className="btn btn-link btn-sm float-right pt-0">
-                              <span>
-                                TIP 0.1 ETH
-                              </span>
-                            </button>
-                          </li>
-                        </ul>
-                      </div>
-                    )
-                  })
-                }
-              </div>
-            </main>
-          </div>
+          <Navbar account={ this.state.account } />
+          <Main posts={ this.state.posts }/>
         </div>
       </div>
     );
